60f45109a2b020e9f632b5c89e49b8b35ab93cad
WangHao <bingoogolapple@gmail.com>
2014-12-21 15:06:13 +0800

    Initial commit

---
+++ flowlayout/src/main/java/cn/bingoogolapple/acvp/flowlayout/MainActivity.java
@@ -0,0 +1,31 @@
+package cn.bingoogolapple.acvp.flowlayout;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+    private FlowLayout mFlowLayout;
+    private String[] mVals = new String[]{"Hello", "AndroidAndroidAndroid", "Welcome", "Button", "HelloHello", "AndroidWelcome", "Welcome", "Button", "HelloWelcome", "Android", "WelcomeWelcome", "Button"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFlowLayout = (FlowLayout) findViewById(R.id.flowlayout);
+        initData();
+    }
+
+    public void initData() {
+        for (int i = 0; i < mVals.length; i++) {
+            Button button = new Button(this);
+            ViewGroup.MarginLayoutParams lp = new ViewGroup.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT);
+            button.setText(mVals[i]);
+            mFlowLayout.addView(button, lp);
+        }
+    }
+}
