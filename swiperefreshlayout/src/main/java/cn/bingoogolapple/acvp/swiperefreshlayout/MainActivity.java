d7bbd8f68c0b120e9403ad86bafe425c20efaef7
WangHao <bingoogolapple@gmail.com>
2014-12-22 18:59:13 +0800

    add SwipeRefreshLayout

---
+++ swiperefreshlayout/src/main/java/cn/bingoogolapple/acvp/swiperefreshlayout/MainActivity.java
@@ -0,0 +1,53 @@
+package cn.bingoogolapple.acvp.swiperefreshlayout;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout mSwipeLayout;
+    private ListView mListView;
+    private ArrayList<String> list = new ArrayList<String>();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listview);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getData()));
+
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeLayout.setOnRefreshListener(this);
+        mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    private ArrayList<String> getData() {
+        list.add("Hello");
+        list.add("This is stormzhang");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        return list;
+    }
+
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeLayout.setRefreshing(false);
+            }
+        }, 5000);
+    }
+}
