3bf533a11314145fae3b7027ae9b4cfe17b06be1
WangHao <bingoogolapple@gmail.com>
2015-01-05 15:07:08 +0800

    add ClearEditText

---
+++ clearedittext/src/main/java/cn/bingoogolapple/acvp/clearedittext/ClearEditText.java
@@ -0,0 +1,79 @@
+package cn.bingoogolapple.acvp.clearedittext;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+public class ClearEditText extends EditText implements TextWatcher, View.OnFocusChangeListener {
+    private Drawable mClearDrawable;
+
+    public ClearEditText(Context context) {
+        this(context, null);
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.editTextStyle);
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            mClearDrawable = getResources().getDrawable(R.drawable.clear_x);
+        }
+        mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
+        setClearIconVisible(false);
+        setOnFocusChangeListener(this);
+        addTextChangedListener(this);
+    }
+
+    protected void setClearIconVisible(boolean visible) {
+        Drawable right = visible ? mClearDrawable : null;
+        setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (getCompoundDrawables()[2] != null) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                boolean touchable = event.getX() > (getWidth() - getPaddingRight() - mClearDrawable.getIntrinsicWidth()) && (event.getX() < getWidth() - getPaddingRight());
+                if (touchable) {
+                    this.setText("");
+                }
+            }
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+    }
+
+    @Override
+    public void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        setClearIconVisible(text.length() > 0);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+}
