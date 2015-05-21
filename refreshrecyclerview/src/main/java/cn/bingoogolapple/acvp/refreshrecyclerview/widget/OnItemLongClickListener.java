ace263a249c7d7fef28274414625cbe8038dc796
王浩 <bingoogolapple@gmail.com>
2015-05-21 18:30:29 +0800

    add refreshrecyclerview

---
+++ refreshrecyclerview/src/main/java/cn/bingoogolapple/acvp/refreshrecyclerview/widget/OnItemLongClickListener.java
@@ -0,0 +1,7 @@
+package cn.bingoogolapple.acvp.refreshrecyclerview.widget;
+
+import android.view.View;
+
+public interface OnItemLongClickListener {
+    boolean onItemLongClick(View v, int position);
+}
