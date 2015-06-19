a4d0bb5f58f62b3ba953d746ef50d7f17ef099e8
bingoogolapple <bingoogolapple@gmail.com>
2015-06-19 09:15:53 +0800

    add ViewPagerCompat

--- vpanim/src/main/java/cn/bingoogolapple/acvp/vpanim/MainActivity.java
+++ vpanim/src/main/java/cn/bingoogolapple/acvp/vpanim/MainActivity.java
@@ -1,17 +1,49 @@
 package cn.bingoogolapple.acvp.vpanim;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
-    private ViewPager mViewPager;
+    private ViewPagerCompat mViewPager;
+    private int[] mImgIds = new int[]{R.mipmap.guide_1, R.mipmap.guide_2, R.mipmap.guide_3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPagerCompat) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return mImgIds.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(mImgIds[position]);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                container.addView(imageView);
+                return imageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        });
+        mViewPager.setPageTransformer(true, new DepthPageTransformer());
     }
 
 }
---
+++ vpanim/src/main/java/cn/bingoogolapple/acvp/vpanim/ZoomOutPageTransformer.java
@@ -0,0 +1,49 @@
+package cn.bingoogolapple.acvp.vpanim;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+import com.nineoldandroids.view.ViewHelper;
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:15/6/19 上午8:42
+ * 描述:
+ */
+public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.85f;
+    private static final float MIN_ALPHA = 0.5f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+        int pageHeight = view.getHeight();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            ViewHelper.setAlpha(view, 0);
+        } else if (position <= 1) { // [-1,1]
+            // Modify the default slide transition to shrink the page as well
+            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+            if (position < 0) {
+                ViewHelper.setTranslationX(view, horzMargin - vertMargin / 2);
+            } else {
+                ViewHelper.setTranslationX(view, -horzMargin + vertMargin / 2);
+            }
+
+            // Scale the page down (between MIN_SCALE and 1)
+            ViewHelper.setScaleX(view, scaleFactor);
+            ViewHelper.setScaleY(view, scaleFactor);
+
+            // Fade the page relative to its size.
+            ViewHelper.setAlpha(view, MIN_ALPHA +
+                    (scaleFactor - MIN_SCALE) /
+                            (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            ViewHelper.setAlpha(view, 0);
+        }
+    }
+}
