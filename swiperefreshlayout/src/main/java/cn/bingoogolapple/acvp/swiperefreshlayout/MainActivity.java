a2a41f553ea02e1f5a7cda3b3cad6f9cfea33cea
bingoogolapple <bingoogolapple@gmail.com>
2015-09-27 23:32:09 +0800

    修改support包版本，查看SwipeRefreshLayout源码

--- swiperefreshlayout/src/main/java/cn/bingoogolapple/acvp/swiperefreshlayout/MainActivity.java
+++ swiperefreshlayout/src/main/java/cn/bingoogolapple/acvp/swiperefreshlayout/MainActivity.java
@@ -3,17 +3,17 @@ package cn.bingoogolapple.acvp.swiperefreshlayout;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private SwipeRefreshLayout mSwipeLayout;
     private ListView mListView;
-    private ArrayList<String> mDatas = new ArrayList<String>();
+    private ArrayList<String> mDatas = new ArrayList<>();
     private BaseAdapter mAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +21,7 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
         setContentView(R.layout.activity_main);
 
         mListView = (ListView) findViewById(R.id.listview);
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getData());
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getData());
         mListView.setAdapter(mAdapter);
 
         mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
@@ -32,7 +32,7 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
     }
 
     private ArrayList<String> getData() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 9; i++) {
             mDatas.add("" + i);
         }
         return mDatas;
